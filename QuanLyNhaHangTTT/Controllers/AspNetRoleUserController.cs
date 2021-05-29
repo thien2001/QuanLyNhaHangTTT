93a3aced7291c33d1ecfd83303203638839d9c86
197PM21985 - HUỲNH PHÚC THIỆN - K25T-PM1 <thien.197pm21985@vanlanguni.vn>
2021-05-29 17:58:49 +0700

    Thêm Role cho người dùng

---
+++ QuanLyNhaHangTTT/Controllers/AspNetRoleUserController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyNhaHangTTT.Models;
+
+namespace QuanLyNhaHangTTT.Controllers
+{
+    public class AspNetRoleUserController : Controller
+    {
+        private CT25Team111Entities db = new CT25Team111Entities();
+
+        // GET: AspNetRoles/Create
+        public ActionResult Create( string roleId)
+        {
+            ViewBag.Role = db.AspNetRoles.Find(roleId);
+            ViewBag.Users = new SelectList(db.AspNetUsers, "Id", "UserName");
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(string roleId , string userId)
+        {
+            var role = db.AspNetRoles.Find(roleId);
+            var user = db.AspNetUsers.Find(userId);
+
+            role.AspNetUsers.Add(user);
+            db.Entry(role).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "AspNetRoles");
+        }
+
+        
+        // GET: AspNetRoles/Delete/5
+        public ActionResult Delete(string roleId, string userId)
+        {
+            var role = db.AspNetRoles.Find(roleId);
+            var user = db.AspNetUsers.Find(userId);
+
+            role.AspNetUsers.Remove(user);
+            db.Entry(role).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "AspNetRoles");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
