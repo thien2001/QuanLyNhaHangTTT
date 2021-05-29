d0ab450c3a2283ec20988169ace96d2177248e3f
197PM21985 - HUỲNH PHÚC THIỆN - K25T-PM1 <thien.197pm21985@vanlanguni.vn>
2021-05-29 22:58:39 +0700

    Thêm templates và trang Index2 cho khách hàng unit test index 1 2

---
+++ QuanLyNhaHangTTT.Tests/Controllers/SanphamsControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuanLyNhaHangTTT.Controllers;
+using QuanLyNhaHangTTT.Models;
+
+namespace QuanLyNhaHangTTT.Tests.Controllers
+{
+    [TestClass]
+    public class SanPhamControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var controller = new SanphamsController();
+
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+
+            var model = result.Model as List<Sanpham>;
+            Assert.IsNotNull(model);
+
+            var db = new CT25Team111Entities();
+            Assert.AreEqual(db.Sanphams.Count(), model.Count);
+        }
+
+        [TestMethod]
+        public void TestIndex2()
+        {
+            var controller = new SanphamsController();
+
+            var result = controller.Index2() as ViewResult;
+            Assert.IsNotNull(result);
+
+            var model = result.Model as List<Sanpham>;
+            Assert.IsNotNull(model);
+
+            var db = new CT25Team111Entities();
+            Assert.AreEqual(db.Sanphams.Count(), model.Count);
+        }
+    }
+}
