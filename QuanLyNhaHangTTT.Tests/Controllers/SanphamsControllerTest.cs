1f2a6f05684e862dbd11225c3103a5dad14fd884
197PM21985 - HUỲNH PHÚC THIỆN - K25T-PM1 <thien.197pm21985@vanlanguni.vn>
2021-05-30 16:44:30 +0700

    Thêm tạo sản phẩm với hình và 1 ít uint test CreateG và CreateP

--- QuanLyNhaHangTTT.Tests/Controllers/SanphamsControllerTest.cs
+++ QuanLyNhaHangTTT.Tests/Controllers/SanphamsControllerTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Moq;
+using System;
+using System.Web;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
@@ -39,6 +41,45 @@ namespace QuanLyNhaHangTTT.Tests.Controllers
 
             var db = new CT25Team111Entities();
             Assert.AreEqual(db.Sanphams.Count(), model.Count);
+        }
+        [TestMethod]
+        public void TestCreateG()
+        {
+            var controller = new SanphamsController();
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestCreateP()
+        {
+            var rand = new Random();
+            var product = new Sanpham
+            {
+                Tên_món_ăn = rand.NextDouble().ToString(),
+                Số_lượng = -rand.Next(),
+                Giá_tiền = -rand.Next()
+            };
+            var controller = new SanphamsController();
+
+            var result0 = controller.Create( product, null) as ViewResult;
+            Assert.IsNotNull(result0);
+            Assert.AreEqual("Giá tiền phải lớn hơn 0", controller.ModelState["Giá_tiền"].Errors[0].ErrorMessage);
+            //product.Giá_tiền = -product.Giá_tiền;
+            //controller.ModelState.Clear();
+
+            Assert.AreEqual("Số lượng phải lớn hơn 0", controller.ModelState["Số_lượng"].Errors[0].ErrorMessage);
+            //product.Số_lượng = -product.Số_lượng;
+            //controller.ModelState.Clear();
+
+            //result0 = controller.Create(product, null) as ViewResult;
+            //Assert.IsNotNull(result0);
+            //Assert.AreEqual("hình ảnh không được tìm thấy!", controller.ModelState[""].Errors[0].ErrorMessage);
+            
+            //var picture = new Mock<HttpPostedFileBase>();
+            //var result1 = controller.Create(product, picture.Object);
         }
     }
 }
